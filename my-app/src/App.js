f5b8fba1d20609916bc34b1e1abeb934ea792bd7
chandan <chandantech56@gmail.com>
2025-02-10 16:33:06 +0530

    React

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import './Component/FunctionalComponent';
+import FunctionalComponent from './Component/FunctionalComponent';
+
+
+import ClassComponent from './Component/ClassComponent';
+
+
+function App() {
+  return (
+    
+      <>
+      <h1>Hello Im a React</h1> 
+      <h4>javascript Library</h4>
+      <p>React is a Single page Website</p>
+      <FunctionalComponent/>
+      <ClassComponent/>
+
+      </>
+    
+  );
+}
+
+
+export default App;
---
+++ my-app/src/Component/FunctionalComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ClassComponent extends React.Component{
+    render(){
+        return(
+            <>
+            <h1> I am a Functional Component</h1>
+            </>
+        )
+    }
+}
+export default ClassComponent
